bcc9f31868fff82d7c917a4fa8d4701e9d5051c5
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-04-12 19:11:57 -0300

    Basic visual function working!

--- lua/frame-it/frameme.lua
+++ lua/frame-it/frameme.lua
@@ -43,4 +43,60 @@ function M.FrameMe(style, ft)
 	end
 end
 
+function FrameMeVisual()
+	-- local style = "rounded"
+	local ft = "lua"
+
+	local comment_marker, comment_match = comment.get_comment_style(ft)
+	if comment_marker == nil then
+		print("Filetype not supported", ft)
+		return
+	end
+
+	-- local hline, vline, topleft, topright, botleft, botright = border.get_frame(style)
+
+	local start_pos = vim.fn.getpos("'<")[2]
+	local end_pos = vim.fn.getpos("'>")[2]
+
+	local lines = vim.api.nvim_buf_get_lines(0, start_pos - 1, end_pos, false)
+
+	local prefixes = {}
+	local text_lines = {}
+	-- Check that every line is a comment.
+	for i, line in ipairs(lines) do
+		if not line:match(comment_match) then
+			print "Not a comment."
+			return
+		end
+		local prefix = line:match(comment_match)
+		local text = line:gsub(comment_match, "")
+		prefixes[i] = prefix
+		text_lines[i] = text
+	end
+
+	-- Get the maximum width.
+	local width = 0
+	for _, line in ipairs(text_lines) do
+		local cols = columns.get_columns(line)
+		if cols > width then
+			width = cols
+		end
+	end
+
+	-- Add a space at the end.
+	width = width + 1
+
+	-- Right pad the strings
+	for i, line in ipairs(text_lines) do
+		text_lines[i] = string.format(string.format("%%-%ds", width), line)
+	end
+
+	-- Frame'em
+	for i, line in ipairs(text_lines) do
+		lines[i] = prefixes[i] .. "@" .. line .. "@"
+	end
+
+	vim.api.nvim_buf_set_lines(0, start_pos - 1, end_pos, false, lines)
+end
+
 return M
