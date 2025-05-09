7371a47fbf464605672d0694d777c886d47665e3
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-05-08 21:27:30 -0300

    Add user commands.

--- plugin/frame-it.lua
+++ plugin/frame-it.lua
@@ -48,3 +48,29 @@ Enjoy!
 --]]
 
 require "frame-it"
+
+-- TODO: Add a setup option if the user doesn't want these commands created and prefer
+-- to invoke the Lua functions directly or create keymaps to them.
+
+-- ┌───────────────────────┐
+-- │ Normal mode commands. │
+-- └───────────────────────┘
+vim.api.nvim_create_user_command('FrameMeSharp', FrameMeSharp, {})
+vim.api.nvim_create_user_command('FrameMeRounded', FrameMeRounded, {})
+vim.api.nvim_create_user_command('FrameMeDotted', FrameMeDotted, {})
+vim.api.nvim_create_user_command('FrameMeDottedRounded', FrameMeDottedRounded, {})
+vim.api.nvim_create_user_command('FrameMeDottedFat', FrameMeDottedFat, {})
+vim.api.nvim_create_user_command('FrameMeFat', FrameMeFat, {})
+vim.api.nvim_create_user_command('FrameMeDouble', FrameMeDouble, {})
+
+-- TODO: Fix the call to visual functions.
+-- ┌───────────────────────┐
+-- │ Visual mode commands. │
+-- └───────────────────────┘
+vim.api.nvim_create_user_command('FrameMeSharpVisual', FrameMeSharpVisual, {})
+vim.api.nvim_create_user_command('FrameMeRoundedVisual', FrameMeRoundedVisual, {})
+vim.api.nvim_create_user_command('FrameMeDottedVisual', FrameMeDottedVisual, {})
+vim.api.nvim_create_user_command('FrameMeDottedRoundedVisual', FrameMeDottedRoundedVisual, {})
+vim.api.nvim_create_user_command('FrameMeDottedFatVisual', FrameMeDottedFatVisual, {})
+vim.api.nvim_create_user_command('FrameMeFatVisual', FrameMeFatVisual, {})
+vim.api.nvim_create_user_command('FrameMeDoubleVisual', FrameMeDoubleVisual, {})
