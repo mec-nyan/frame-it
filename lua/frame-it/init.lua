3ef1da09d38285e8e6a6bb353c09fc249c5c1a2b
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-05-12 13:34:15 -0300

    Refactor call to functions:
    Add "opts" parameter so we can use the wrapper "FrameMe" to call the
    same function in normal or visual mode.

--- lua/frame-it/init.lua
+++ lua/frame-it/init.lua
@@ -1,125 +1,61 @@
 local frameme = require "frame-it.frameme"
 local borders = require "frame-it.borders"
 
+local M = {}
+
 -- Line-wise (normal mode) functions:
 
 -- ┌──────────────┐
 -- │ FrameMeSharp │
 -- └──────────────┘
-function FrameMeSharp()
-	frameme.FrameMe(borders.sharp, vim.bo.filetype)
+M.FrameMeSharp = function (opts)
+	frameme.FrameMe(borders.sharp, vim.bo.filetype, opts)
 end
 
 -- ╭────────────────╮
 -- │ FrameMeRounded │
 -- ╰────────────────╯
-function FrameMeRounded()
-	frameme.FrameMe(borders.rounded, vim.bo.filetype)
+M.FrameMeRounded = function (opts)
+	frameme.FrameMe(borders.rounded, vim.bo.filetype, opts)
 end
 
 -- ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
 -- ┆ FrameMeDotted ┆
 -- └╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
-function FrameMeDotted()
-	frameme.FrameMe(borders.dotted, vim.bo.filetype)
+M.FrameMeDotted =function  (opts)
+	frameme.FrameMe(borders.dotted, vim.bo.filetype, opts)
 end
 
 -- ╭╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╮
 -- ┆ FrameMeDottedRounded ┆
 -- ╰╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╯
-function FrameMeDottedRounded()
-	frameme.FrameMe(borders.dotted_rounded, vim.bo.filetype)
+M.FrameMeDottedRounded =function  (opts)
+	frameme.FrameMe(borders.dotted_rounded, vim.bo.filetype, opts)
 end
 
 -- ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓
 -- ┇ FrameMeDottedFat ┇
 -- ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛
-function FrameMeDottedFat()
-	frameme.FrameMe(borders.dotted_fat, vim.bo.filetype)
+M.FrameMeDottedFat =function  (opts)
+	frameme.FrameMe(borders.dotted_fat, vim.bo.filetype, opts)
 end
 
 -- ┏━━━━━━━━━━━━┓
 -- ┃ FrameMeFat ┃
 -- ┗━━━━━━━━━━━━┛
-function FrameMeFat()
-	frameme.FrameMe(borders.fat, vim.bo.filetype)
+M.FrameMeFat =function  (opts)
+	frameme.FrameMe(borders.fat, vim.bo.filetype, opts)
 end
 
 -- ╔═══════════════╗
 -- ║ FrameMeDouble ║
 -- ╚═══════════════╝
-function FrameMeDouble()
-	frameme.FrameMe(borders.double, vim.bo.filetype)
+M.FrameMeDouble =function  (opts)
+	frameme.FrameMe(borders.double, vim.bo.filetype, opts)
 end
 
--- Block-wise (visual mode) functions:
+M.__FrameMeVisual = function (style, lang, opts) frameme.FrameMeVisual(style, lang, opts) end
 
--- ┌────────────────────┐
--- │ FrameMeSharpVisual │
--- └────────────────────┘
-function FrameMeSharpVisual()
-	frameme.FrameMeVisual(borders.sharp, vim.bo.filetype)
-end
-
--- ╭──────────────────────╮
--- │ FrameMeRoundedVisual │
--- ╰──────────────────────╯
-function FrameMeRoundedVisual()
-	frameme.FrameMeVisual(borders.rounded, vim.bo.filetype)
-end
-
--- ┌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┐
--- ┆ FrameMeDottedVisual ┆
--- └╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌┘
-function FrameMeDottedVisual()
-	frameme.FrameMeVisual(borders.dotted, vim.bo.filetype)
-end
-
--- ╭╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╮
--- ┆ FrameMeDottedRoundedVisual ┆
--- ╰╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╌╯
-function FrameMeDottedRoundedVisual()
-	frameme.FrameMeVisual(borders.dotted_rounded, vim.bo.filetype)
-end
-
--- ┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┓
--- ┇ FrameMeDottedFatVisual ┇
--- ┗╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍┛
-function FrameMeDottedFatVisual()
-	frameme.FrameMeVisual(borders.dotted_fat, vim.bo.filetype)
-end
-
--- ┏━━━━━━━━━━━━━━━━━━┓
--- ┃ FrameMeFatVisual ┃
--- ┗━━━━━━━━━━━━━━━━━━┛
-function FrameMeFatVisual()
-	frameme.FrameMeVisual(borders.fat, vim.bo.filetype)
-end
-
--- ╔═════════════════════╗
--- ║ FrameMeDoubleVisual ║
--- ╚═════════════════════╝
-function FrameMeDoubleVisual()
-	frameme.FrameMeVisual(borders.double, vim.bo.filetype)
-end
-
-M = {
-	__FrameMe = function(style, lang) frameme.FrameMe(style, lang) end,
-	FrameMeSharp = FrameMeSharp,
-	FrameMeRounded = FrameMeRounded,
-	FrameMeDotted = FrameMeDotted,
-	FrameMeDottedRounded = FrameMeDottedRounded,
-	FrameMeDottedFat = FrameMeDottedFat,
-	FrameMeFat = FrameMeFat,
-	FrameMeDouble = FrameMeDouble,
-
-	FrameMeSharpVisual = FrameMeSharpVisual,
-	FrameMeRoundedVisual = FrameMeRoundedVisual,
-	FrameMeDottedVisual = FrameMeDottedVisual,
-	FrameMeDottedRoundedVisual = FrameMeDottedRoundedVisual,
-	FrameMeDottedFatVisual = FrameMeDottedFatVisual,
-	FrameMeFatVisual = FrameMeFatVisual,
-	FrameMeDoubleVisual = FrameMeDoubleVisual,
-}
+M.__FrameMe = function(style, lang) frameme.FrameMe(style, lang) end
 
 return M
